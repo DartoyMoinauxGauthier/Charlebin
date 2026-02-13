7157018651112d1e1fd6590ec32cefb9049eec44
Gauthier <e88969u@etu.univ-lorraine.fr>
2026-02-13 08:58:16 +0100

    Initial: Importation des sources de PrivateBin

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+export PATH="$PATH:$PWD/vendor/bin"
+echo 'export PATH="$PATH:$HOME/.composer/vendor/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:$PWD/vendor/bin"' >> ~/.bashrc
+ln -s ./conf.sample.php cfg/conf.php
+composer install --no-dev --optimize-autoloader
+
+# for PHP unit testing
+composer require --global google/cloud-storage
+
+sudo chmod a+x "$(pwd)" && sudo rm -rf /var/www/html && sudo ln -s "$(pwd)" /var/www/html
+
+npm install --global nyc
